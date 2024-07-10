0e60566b7235638f8c374dc4a881bc8d34e86e33
hashimxkhan <khan521@purdue.edu>
2024-07-10 17:03:59 +0500

    Modified web scraping logic

--- web-scraping/webScraping2.py
+++ web-scraping/webScraping2.py
@@ -1,6 +1,7 @@
 from playwright.sync_api import sync_playwright
 import re 
 import time 
+from pymongo import MongoClient
 
 #-------------------------------------------------------------------------------------------------------------- 
 #curret problems: extract pics (after first listing). go to next page and rerun entire script till all pages are done.  
@@ -9,8 +10,9 @@ import time
 #next step: once u can get data from all pages with a single script run, implement captcha and translation api. (then add database and think about formatting)
  
 #-------------------------------------------------------------------------------------------------------------- 
-
-def run(playwright):
+city_name = "Berlin"
+max_rent = "10000"
+def run(playwright, city_name):
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context()
     
@@ -34,8 +36,8 @@ def run(playwright):
     try: 
         accept_cookies(1)
         # Interact with the input box
-        city_name = input("Enter city name: ")
-        max_rent = input("Enter max rent: ")
+        # city_name = input("Enter city name: ")
+        # max_rent = input("Enter max rent: ")
 
         input_box = page.wait_for_selector("#autocompinp")
         rent_box = page.wait_for_selector("#rMax")
@@ -87,7 +89,7 @@ def run(playwright):
             page_number += 1
             formatted_url = (f"{url_broken[0]}{page_url_format[0]}.{str(page_number)}.html{url_broken[1]}")
             
-
+            apartments = []
             for index, listing in enumerate(listings):
                 try: 
                     if "vermietet" in listing.text_content(): 
@@ -99,31 +101,55 @@ def run(playwright):
                     # #     # continue_to_pics = page2.wait_for_selector(".pull-right.mb20r")
                     # #     # continue_to_pics.click()
 
-                    
-                    listing_url = f"https://www.wg-gesucht.de{new_page_link}"
-                    free_from = listing.wait_for_selector(".col-xs-5.text-center").inner_text()
-                    address = page2.wait_for_selector("#main_column > div:nth-child(6) > div > div > div > div:nth-child(1) > div > div > a > span").inner_text()
-                    room_details = page2.query_selector(".pl15.mb15").inner_text().split("Sprache/n:")[0]
+                    apartment = {
+                    "listing_url": f"https://www.wg-gesucht.de{new_page_link}",
+                    "free_from": listing.wait_for_selector(".col-xs-5.text-center").inner_text(),
+                    "address": page2.wait_for_selector("#main_column > div:nth-child(6) > div > div > div > div:nth-child(1) > div > div > a > span").inner_text(),
+                    "room_size": listing.query_selector(".col-xs-3.text-right").inner_text(),
+                    #"room_details": page2.query_selector(".pl15.mb15").inner_text().split("Sprache/n:")[0],
                     # room_details = page2.wait_for_selector(".pl15.mb15")
-                    apartment_poster = listing.wait_for_selector(".ml5").inner_text()
-                    apartment_rent = listing.query_selector(".col-xs-3").inner_text()
-                
-                    apartment_name = listing.query_selector(".truncate_title").inner_text()
-                
-                
+                    "apartment_poster": listing.wait_for_selector(".ml5").inner_text(),
+                    "apartment_rent": listing.query_selector(".col-xs-3").inner_text(),
+                    "apartment_name": listing.query_selector(".truncate_title").inner_text()
+                    }
+                    apartments.append(apartment)
+                    print("Listing URL:", apartment["listing_url"])
+                    print("Free from:", apartment["free_from"])
+                    print("Address:", apartment["address"])
+                    print("Room size:", apartment["room_size"])
+                    print("Apartment poster:", apartment["apartment_poster"])
+                    print("Apartment rent:", apartment["apartment_rent"])
+                    print("Apartment name:", apartment["apartment_name"])
+                    print("-" * 50)  # Separator between apartments
 
-                    print(f"\n\nApartment Name: {apartment_name}\nApartment Rent: {apartment_rent}\nApartment Poster: {apartment_poster}\nFree from: {free_from}\nAddress: {address}\nRoom Details: {room_details}\n\n")
+                #    print(f"\n\nApartment Name: {apartment_name}\nApartment Rent: {apartment_rent}\nApartment Poster: {apartment_poster}\nFree from: {free_from}\nAddress: {address}\nRoom Details: {room_details}\n\n")
                 except Exception as e:
                     print(f"Error in listing {index}: {e}")
             
 
     except Exception as e: 
         print(f"error {e}") #change this to 'return' at the end
-    finally: 
-        browser.close()
+    #finally: 
+        #browser.close()
+    return apartments
 
 
+top_cities = [
+   "Berlin", "Hamburg", "München", "Köln", "Frankfurt am Main", "Stuttgart", "Düsseldorf", "Dortmund",
+    "Essen", "Leipzig", "Bremen", "Dresden", "Hannover", "Nürnberg", "Duisburg", "Bochum",
+    "Wuppertal", "Bielefeld", "Bonn", "Mannheim", "Karlsruhe", "Wiesbaden", "Münster", "Augsburg",
+    "Gelsenkirchen", "Mönchengladbach", "Braunschweig", "Chemnitz", "Kiel", "Aachen", "Halle (Saale)",
+    "Magdeburg", "Freiburg im Breisgau", "Krefeld", "Lübeck", "Oberhausen", "Erfurt", "Mainz", "Rostock",
+    "Kassel", "Hagen", "Saarbrücken", "Hamm", "Mülheim an der Ruhr", "Potsdam", "Ludwigshafen am Rhein", "Oldenburg",
+    "Leverkusen", "Osnabrück", "Solingen", "Heidelberg", "Herne", "Neuss", "Darmstadt", "Paderborn",
+    "Regensburg", "Ingolstadt", "Würzburg", "Wolfsburg", "Offenbach am Main", "Ulm", "Heilbronn", "Pforzheim",
+    "Göttingen", "Bottrop", "Trier", "Recklinghausen", "Reutlingen", "Bremerhaven", "Koblenz",
+    "Bergisch Gladbach", "Jena", "Remscheid", "Erlangen", "Moers", "Siegen", "Hildesheim",
+    "Salzgitter", "Cottbus", "Kaiserslautern", "Gütersloh", "Schwerin", "Witten", "Gera", "Iserlohn",
+    "Zwickau", "Düren", "Esslingen am Neckar", "Ratingen", "Marl", "Lünen", "Hanau", "Velbert", "Dessau-Roßlau",
+    "Lüdenscheid", "Viersen", "Flensburg", "Gießen", "Lüneburg", "Worms", "Wilhelmshaven", "Neumünster",
+    "Castrop-Rauxel", "Hattingen", "Sankt Augustin", "Grevenbroich", "Herten", "Troisdorf", "Celle"
+]
 
 
-with sync_playwright() as playwright:
-    run(playwright)
+    
