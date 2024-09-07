9801d4fd231379071780e816fc86d11b96dce753
hamza <hamzasohail345@gmail.com>
2024-09-07 14:39:17 +0500

    added lightmode darkmode

--- roomlocator-react-app/src/components/ResultsPage.js
+++ roomlocator-react-app/src/components/ResultsPage.js
@@ -7,6 +7,13 @@ import ListingsTable from './create-table';
 
 function ResultsPage() {
   
+    const [theme, setTheme] = useState('light-theme'); // Default theme
+  
+    const toggleTheme = () => {
+      setTheme(prevTheme => prevTheme === 'dark-theme' ? 'light-theme' : 'dark-theme');
+    };
+  
+
   const location = useLocation();
   const [city, setCity] = useState('');
   const [minPrice, setMinPrice] = useState('');
@@ -36,7 +43,7 @@ function ResultsPage() {
  ListingsData = FetchData(city, minPrice, maxPrice) //this is a list with dictionaries which have rent name poster free from url other details room size
  
   return (
-    <div className='results-page'>
+    <div className={`results-page ${theme}`}>
       <header className="header">
         <div>RoomLocator <div className="small">Deutschland</div></div>
         <div className="right">
@@ -44,18 +51,31 @@ function ResultsPage() {
           <a href="/about">About</a>
         </div>
       </header>
+      <div className={`body-results ${theme}`}>
 
-      <div className='table'> 
+        <div className='controls'>
+         
+         <div className='searchbutton'>
+          <input type='text' placeholder="Enter a city or a town.."/>
+          <button type='button'>Search</button>
+         </div>
+         <button type='button' className='control-button' onClick={toggleTheme}>Theme</button>
+         <button type='button' className='control-button'>Sort</button>
+
+        </div>
+        <div className='table'> 
         <ListingsTable data={data}></ListingsTable>
       </div>
-
-      <footer className="footer results-page">
+      
+      </div>
+      
+      <footer className={`footer results-page ${theme}`}>
         created by
         <a href="https://github.com/btyneo" target="_blank" rel="noopener noreferrer"> btyneo </a> &
         <a href="https://github.com/hashimxkhan" target="_blank" rel="noopener noreferrer"> hashimxkhan </a>
       </footer>
     </div>
   );
-}
+};
 
 export default ResultsPage;
